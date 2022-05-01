2787fb94c20136b6e406256be947d0f438a894ac
ismetsovetski <104693477+ismetsovetski@users.noreply.github.com>
2022-05-02 00:12:55 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5251276410:AAFICSji0aWb09I51Yjw7wDQ3pyGA9FD0cg"
+TOKEN = "5114584670:AAE4EwC5Bv_6_GlnGg2nXnLPI_rMnwotpR8"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
